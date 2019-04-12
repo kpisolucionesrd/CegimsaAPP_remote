154b3d8f3851c725598677d3f9bacb6cd476fb44
Kelvin Melendez <kpisolucionesrd@gmail.com>
2019-04-12 00:31:37 -0700

    Cambios

--- pantallas/galeriaIMG.js
+++ pantallas/galeriaIMG.js
@@ -67,7 +67,8 @@ export default class GaleriaImagenes extends Component<Props> {
   };
 
   prueba=async()=>{
-    alert(JSON.stringify(this.state.media[0]["photo"]));
+    alert(await AsyncStorage.getItem("objetoImagenesJugador"));
+    //alert(JSON.stringify(this.state.media[0]["photo"]));
   }
 
   render() {
