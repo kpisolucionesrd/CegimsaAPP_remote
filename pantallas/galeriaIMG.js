23ab6253f7d82934f16ef7bda845e2271b76bcc0
Kelvin Melendez <kpisolucionesrd@gmail.com>
2019-04-15 02:36:06 -0700

    Cambios

--- pantallas/galeriaIMG.js
+++ pantallas/galeriaIMG.js
@@ -82,7 +82,7 @@ export default class GaleriaImagenes extends Component<Props> {
   prueba=async()=>{
     var objetoImagenes={};
     var counting=1;
-    alert(this.state.vectorObjetos2);
+    alert(this.state.vectorObjetos2.length);
 
     // await this.state.vectorObjetos2.forEach(img => {
     //   objetoImagenes["img"+counting]=require(img)
