e0587f4c6d0a2bf733019ab6c58cbcb128bc753f
Kelvin Melendez <kpisolucionesrd@gmail.com>
2019-04-17 08:27:11 -0700

    Cambio

--- pantallas/galeriaIMG.js
+++ pantallas/galeriaIMG.js
@@ -198,6 +198,13 @@ const styles = StyleSheet.create({
       },
       shadowOpacity:15
     },
+    backgroundVideo: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
     imgGaleria:{
       width:100,
       height:100,
