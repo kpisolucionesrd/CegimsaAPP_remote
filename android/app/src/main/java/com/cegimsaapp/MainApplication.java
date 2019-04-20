2dea8dd62ea5f5603cdfdf919a3bad9d3d206054
user914850 <user914850@74-80-245-7.ic2net.net>
2019-04-20 13:44:04 -0700

    Cambios

--- android/app/src/main/java/com/cegimsaapp/MainApplication.java
+++ android/app/src/main/java/com/cegimsaapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.cegimsaapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfs.RNFSPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new RNFSPackage(),
